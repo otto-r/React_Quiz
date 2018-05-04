0f2f5066807a3ae906757f2df104e5c0ef3f0978
rikard <rikard.hohlfalt@gmail.com>
2018-05-04 10:00:29 +0200

    Force push

--- Quiz_react/Controllers/QuestionsController.cs
+++ Quiz_react/Controllers/QuestionsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Quiz_react.Models;
 
 namespace Quiz_react.Controllers
 {
+    [Authorize]
     [Produces("application/json")]
     [Route("api/Questions")]
     public class QuestionsController : Controller
